f153afd1a96a2bbad719a2d6a956205541ff5453
Max Girteit <icufishmg@gmail.com>
2021-07-11 09:47:47 -0600

    coords.js and variables.js removed

--- src/client/js/coords.js
+++
@@ -1,8 +0,0 @@
-// const geoAPIKey = "&maxRows=1&username=icufishmg";
-// const geoBaseURL = "http://api.geonames.org/searchJSON?q=";
-// const weatherbitAPIKey = "&key=1d235b160c6c4917b499388daca9bc82";
-// const weatherbitBaseURL = "https://api.weatherbit.io/v2.0/normals?";
-import "./variables";
-
-
-  export { getCoords };
