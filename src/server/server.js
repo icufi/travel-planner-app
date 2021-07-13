e7f9f808b76651c96c933c5f53e48b5412b6a260
Max Girteit <icufishmg@gmail.com>
2021-07-13 13:29:03 -0600

    server linted

--- src/server/server.js
+++ src/server/server.js
@@ -1,50 +1,53 @@
+/* eslint-disable no-undef */
 // Setup empty JS object to act as endpoint for all routes
 projectData = [];
 
 // Require Express to run server and routes
-const express = require("express");
+const express = require('express');
 
 // Start up an instance of app
 const app = express();
 
-/* Middleware*/
+/* Middleware */
 // const bodyParser = require('body-parser'); deprecated
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
-//Here we are configuring express to use body-parser as middle-ware.
+// Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require("cors");
+const cors = require('cors');
+
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
 // Setup Server
 const port = 8000;
-const server = app.listen(port, () =>
-  console.log(`server is listening on port ${port}`)
-);
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-console
+const server = app.listen(port, () => console.log(`server is listening on port ${port}`));
 
-//get and post route for receiving and sending data from app.js
-app.get("/projectdata", (req, res) => res.send(projectData));
-app.post("/travelinfo", saveWeather);
-
-//callback function to save data received from app.js to projectData array
+// callback function to save data received from app.js to projectData array
+// eslint-disable-next-line no-unused-vars
 function saveWeather(req, res) {
-  let data = req.body;
-  console.log(data)
-  let newEntry = {
+  const data = req.body;
+  const newEntry = {
     city: data.city,
     country: data.country,
     highTemp: data.highTemp,
     minTemp: data.minTemp,
     precip: data.precip,
     travelDate: data.travelDate,
-    daysCountdown: data.daysCountdown
+    daysCountdown: data.daysCountdown,
   };
   projectData.push(newEntry);
+  // eslint-disable-next-line no-console
   console.log(projectData);
 }
+
+// get and post route for receiving and sending data from app.js
+app.get('/projectdata', (req, res) => res.send(projectData));
+app.post('/travelinfo', saveWeather);
